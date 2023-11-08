db24cc0bb281b4a145dcbcc3359dd77012eb6f56
SergeInGithub <nyiriserge@gmail.com>
2023-11-08 16:10:55 +0200

    ft(worker): add worker.js file

---
+++ rabbit 2/src/worker.js
@@ -0,0 +1,38 @@
+import amqp from 'amqplib/callback_api.js';
+
+amqp.connect('amqp://localhost:5672', function (error0, connection) {
+  if (error0) {
+    throw error0;
+  }
+
+  connection.createChannel(function (error1, channel) {
+    if (error1) {
+      throw error1;
+    }
+
+    const queue = 'task_queue';
+
+    //* This makes sure the queue is declared before attempting to consume from it
+    channel.assertQueue(queue, {
+      durable: true,
+    });
+    channel.prefetch(1);
+    console.log(' [*] Waiting for messages in %s. To exit press CTRL+C', queue);
+
+    channel.consume(
+      queue,
+      function (msg) {
+        const secs = msg.content.toString().split('.').length - 1;
+
+        console.log(' [x] Received %s', msg.content.toString());
+        setTimeout(function () {
+          console.log(' [x] Done');
+        }, secs * 1000);
+      },
+      {
+        //* automatic acknowledgement mode
+        noAck: false,
+      },
+    );
+  });
+});
